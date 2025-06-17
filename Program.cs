e9f75a7d8e0d2d0521d7beba2e19a4acdf480d50
Ancelia Mathias Patrao <55922585+anceliaa06@users.noreply.github.com>
2025-06-17 21:32:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int reversedNum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                reversedNum = reversedNum * 10 + digit;
+                num /= 10;
+            }
+
+            Console.WriteLine($"Reverse is: {reversedNum}");
+        }
+    }
+}
